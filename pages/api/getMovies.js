21c7134546a55f336c913b5cb8b2634ea57b14ed
shubham-ram <shubhamram2k@gmail.com>
2024-03-10 20:21:48 +0530

    added pagination query in getMovie api

--- pages/api/getMovies.js
+++ pages/api/getMovies.js
@@ -2,15 +2,18 @@ import pool from "@/common/db/connection";
 import { v4 as uuidV4 } from "uuid";
 
 async function handler(req, res) {
-  try {
-    const query = {
-      text: "SELECT * FROM movies",
-    };
-    const resp = await pool.query(query);
-    res.status(200).json({ hasError: false, data: resp?.rows });
-  } catch (error) {
-    res.status(500).json({ hasError: true, msg: error });
-  }
+    try {
+        const { page } = req.query;
+
+        const query = {
+            //   text: "SELECT * FROM movies",
+            text: `SELECT * FROM movies ORDER BY release_year DESC OFFSET ${10 * (page - 1)} ROWS FETCH NEXT 10 ROWS ONLY`
+        };
+        const resp = await pool.query(query);
+        res.status(200).json({ hasError: false, data: resp?.rows });
+    } catch (error) {
+        res.status(500).json({ hasError: true, msg: error });
+    }
 }
 
 export default handler;
