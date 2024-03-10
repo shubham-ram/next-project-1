7a422f6c1750cc0c6cdf82e115e66ded78f3c175
shubham-ram <shubhamram2k@gmail.com>
2024-03-10 20:21:30 +0530

    updated setMovie api

--- pages/api/setMovies.js
+++ pages/api/setMovies.js
@@ -1,49 +1,54 @@
+import { v4 as uuidV4 } from "uuid";
+
 import pool from "@/common/db/connection";
 
-const url =
-  "https://global-movies-api-hub.p.rapidapi.com/movies?limit=100&offset=0&filter%5ByearRange%5D%5B0%5D=2018&filter%5ByearRange%5D%5B1%5D=2024";
+// const url =
+//     "https://global-movies-api-hub.p.rapidapi.com/movies?limit=100&offset=0&filter%5ByearRange%5D%5B0%5D=2018&filter%5ByearRange%5D%5B1%5D=2024";
+
+// const url = 'https://global-movies-api-hub.p.rapidapi.com/movies?limit=120&orderBy=createdAt_DESC&filter%5Blanguage%5D=English&filter%5ByearRange%5D%5B0%5D=2018&filter%5ByearRange%5D%5B1%5D=2024';
+
+const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
 
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "c46653efacmsh2cc4a84301b3da0p185dcajsn91e28211be2c",
-    "X-RapidAPI-Host": "global-movies-api-hub.p.rapidapi.com",
-  },
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': 'c46653efacmsh2cc4a84301b3da0p185dcajsn91e28211be2c',
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+    }
 };
 
 async function handler(req, res) {
-  try {
-    const resp = await fetch(url, options);
-    const jsonResp = await resp.json();
+    try {
+        const resp = await fetch(url, options);
+        const jsonResp = await resp.json();
 
-    const { rows } = jsonResp || {};
+        const prArr = jsonResp.map((ele) => {
+            const { title, description, year, rating, image } = ele;
 
-    const prArr = rows.map((ele) => {
-      const { id, title, description, release, rating, images = [] } = ele;
+            const query = {
+                text: "INSERT INTO movies(id, title, description, poster_url, release_year, rating, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
+                values: [
+                    uuidV4(),
+                    title,
+                    description,
+                    image,
+                    new Date(`${year}`).getFullYear(),
+                    rating,
+                    new Date(),
+                    new Date(),
+                ],
+            };
 
-      const query = {
-        text: "INSERT INTO movies(id, title, description, poster_url, release_date, rating, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
-        values: [
-          id,
-          title,
-          description,
-          images[0]?.publicUrl,
-          release,
-          rating,
-          new Date(),
-          new Date(),
-        ],
-      };
+            return pool.query(query);
+        });
 
-      return pool.query(query);
-    });
+        const prRes = await Promise.allSettled(prArr);
+        // console.log(prRes);
 
-    const prRes = await Promise.allSettled(prArr);
-
-    res.status(200).json({ hasError: false, data: jsonResp });
-  } catch (error) {
-    res.status(500).json({ hasError: true, msg: error });
-  }
+        res.status(200).json({ hasError: false, data: jsonResp });
+    } catch (error) {
+        res.status(500).json({ hasError: true, msg: error });
+    }
 }
 
 export default handler;
